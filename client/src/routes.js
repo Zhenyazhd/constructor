04d470d80e94ab207b7a90d8027761162efc55ed
zhenyazhd <zhenyazhdarkin@gmail.com>
2020-11-18 02:50:52 +0700

    update

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,20 +2,22 @@ import React from 'react'
 import {Switch, Route, Redirect} from 'react-router-dom'
 import {AccountPage} from './pages/AccountPage'
 import {AuthPage} from './pages/AuthPage'
-/*import {Contract} from './pages/Contract'
-<Route path="/contract/:id">
+import {Contract} from './pages/Contract'
+/*<Route path="/contract/:id">
           <Contract />
         </Route> */
 
 export const useRoutes = isAuthenticated => {
 
-  if (isAuthenticated) {//exact for access<Redirect to="/use" /> 
+  if (isAuthenticated) {
     return (
       <Switch>
         <Route path="/account" exact> 
           <AccountPage />
         </Route> 
-        
+        <Route path="/contract/:id">
+          <Contract />
+        </Route>
         <Redirect to="/account" exact/>
       </Switch>
     )
