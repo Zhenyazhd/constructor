f3d1bcf0333cc673a4f9602ecb857957265d64bc
zhenyazhd <zhenyazhdarkin@gmail.com>
2020-11-18 01:27:48 +0700

    update

--- client/src/pages/Deploy.js
+++ client/src/pages/Deploy.js
@@ -7,7 +7,7 @@ import Form from "@rjsf/material-ui"
 
 
 export const Deploy = (props) => {
-  /*  const history = useHistory()  
+  const history = useHistory()  
     const auth = useContext(AuthContext)
     const {request} = useHttp()
 
@@ -19,7 +19,7 @@ export const Deploy = (props) => {
         window.M.updateTextFields()
     }, [])
 
-
+/*  
     const onSubmit_deploy = async ({formData}) => {
         let address
         const web3 = new Web3(Web3.givenProvider,  null, { transactionConfirmationBlocks: 1});
@@ -49,7 +49,7 @@ export const Deploy = (props) => {
             history.push('/')
         } catch (e) { }
     }
-
+*/
     let content
     let dep = `{
       "title": "Do you agree use that account for deploying?",
@@ -68,10 +68,10 @@ export const Deploy = (props) => {
       }
     }`
     dep = JSON.parse(dep)
-
+//onSubmit={onSubmit_deploy}
     if (deployed === false) {
-      content = <Form schema={props.schema} onSubmit={onSubmit_deploy}/>
-    }*/
+      content = <Form schema={props.schema} />
+    }
     
     return (
       <div> 
